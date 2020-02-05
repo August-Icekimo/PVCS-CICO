b52a1ec76788363c4359ccd2801ee58d3b380a54
August-Icekimo <august.icekimo@gmail.com>
2020-02-05 12:16:39 +0800

    chackout with "l"ock  and more annotations

--- pvcs_checkout.groovy
+++ pvcs_checkout.groovy
@@ -52,6 +52,41 @@ def runCommand = {def message, def command ->
     process.getOutputStream().close()
     process.waitFor()
     if (process.exitValue()) {
+        switch ( process.exitValue()) {
+            case "0":
+            println("No problem.")
+            break
+            case "-2":
+            println("PCLI command not found.")
+            break
+            case "-3":
+            println("A non-PCLI related error or a command-specific error.")
+            break
+            case "-6":
+            println("An invalid argument was specified.")
+            break
+            case "-7":
+            println("An argument for a flag that is not needed.")
+            break
+            case "-8":
+            println("A missing argument for a flag.")
+            break
+            case "-9":
+            println("Wrong type was specified for an option's argument.")
+            break
+            case "-10":
+            println("The specified file name cannot be read.")
+            break
+            case "-11":
+            println("A required argument is missing.")
+            break
+            case "-12":
+            println("A security exception occurred.")
+            break
+            case "-2":
+            println("An unknown problem.")
+            break
+        }
         throw new Exception("Command failed with exit code: " + process.exitValue())
     }
 }
@@ -78,15 +113,33 @@ if (!workDir.isDirectory()) {
   
 def command = [pcliPath]
 command << "Get"
-command << "-q"
+// Depose Arg "Quietly ignores nonexistent entities."", we need noise.
+//command << "-qe"
 command << "-y"
+// Args "-o" 
+// Overrides the workfile locations defined in the project and versioned files,
+// and instead uses a hierarchy of directories that mirror the structure and names of the
+// project and subprojects. Note if you do not use this option, any versioned file or
+// project that has an absolute workfile location associated with it will be copied to
+// that workfile location, even if you specify a workspace or use the -a option.
 command << "-o"
+// Args "-bp" 
+// Specifies the base project path to use in calculating workfile locations when
+// the -a option has been specified.
 command << "-bp" + basePath
-command << "-z"
+// Args "-l"
+// Locks the revision of the file you are getting. Optionally, allows you to specify the revision
+// to lock. By default, the default revision defined for the workspace is acted on. Note the revision
+// was assign with the -r args below.
+command << "-l"
 if (id != null) {
     command << "-id" + id
 }
+// Args "-pr" 
+// Sets the current project database for this command execution.
 command << "-pr" + databasePath
+// Args "-r"
+// Specifies the revision, promotion group, or version to act upon.
 if (label != null && label.trim().length() > 0) {
     command << "-r" + label.trim()
 }
@@ -96,9 +149,21 @@ else if (branch != null && branch.trim().length() > 0) {
 else if (promotionGroup != null && promotionGroup.trim().length() > 0) {
     command << "-g" + promotionGroup.trim() 
 }
+//Args "-a" 
+// Specifies an alternate location to place workfiles, rather than the location defined in the
+// workspace. See also the -o and -bp options.
+// For single file checkouts, the alternate location can be either a directory or an alternate
+// name for the file itself. If the specified leafname of the destination path:
+//  Does not exist and the -bp option was not used, the leafname is assumed to be an
+//  alternate filename.
+//  Already exists as a directory, the file will be placed into that directory using its 
+//  original name.
+//  If the -bp option is used, the leafname is always assumed to be a directory; additional
+//  subdirectories may be created depending on how the -bp option was used.
 command << "-a" + workDir.absolutePath
 command << projectPath
-
+//Args "-z" Includes versioned files in subprojects.
+command << "-z" + "/"
 //------------------------------------------------------------------------------
 // EXECUTE
 //------------------------------------------------------------------------------
