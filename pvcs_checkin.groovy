77bbee4ca8f51688bf987362f8814400d01dd197
August-Icekimo <august.icekimo@gmail.com>
2020-02-05 15:13:13 +0800

    PCLI Put command Args & annotations

--- pvcs_checkin.groovy
+++ pvcs_checkin.groovy
@@ -28,7 +28,8 @@ def promotionGroup = props['promotionGroup'];
 def cleanWorkspace = props['cleanWorkspace']?.toBoolean();
 def user           = props['user'];
 def password       = props['password'];
-
+def changeDescription   = props['changeDescription'];   //TODO
+def unlockPath           = props['unlockPath'];         //TODO
 def id = null
 if (user != null && user.trim().length() > 0) {
     if (password != null && password.trim().length() > 0) {
@@ -52,12 +53,49 @@ def runCommand = {def message, def command ->
     process.getOutputStream().close()
     process.waitFor()
     if (process.exitValue()) {
-        throw new Exception("Command failed with exit code: " + process.exitValue())
+    // express exit value in pcli references. 
+        switch ( process.exitValue()) {
+            case "0":
+            println("No problem.")
+            break
+            case "-2":
+            println("PCLI command not found.")
+            break
+            case "-3":
+            println("A non-PCLI related error or a command-specific error.")
+            break
+            case "-6":
+            println("An invalid argument was specified.")
+            break
+            case "-7":
+            println("An argument for a flag that is not needed.")
+            break
+            case "-8":
+            println("A missing argument for a flag.")
+            break
+            case "-9":
+            println("Wrong type was specified for an option's argument.")
+            break
+            case "-10":
+            println("The specified file name cannot be read.")
+            break
+            case "-11":
+            println("A required argument is missing.")
+            break
+            case "-12":
+            println("A security exception occurred.")
+            break
+            case "-13":
+            println("An unknown problem.")
+            break
+        }
+        throw new Exception("GET Command failed with exit code: " + process.exitValue())
     }
 }
 
 //------------------------------------------------------------------------------
 // PREPARE WORKING DIRECTORY
+// MAY DEPRECATED in future
 //------------------------------------------------------------------------------
 
 if (cleanWorkspace && workDir.isDirectory()) {
@@ -78,17 +116,41 @@ if (!workDir.isDirectory()) {
   
 def command = [pcliPath]
 command << "Put"
-//command << "-vTEST"
+
+//Args "-v"
+// Assigns a version label to the new revision. Note, you can use the -yv or -nv option to
+// automatically answer Yes or No to prompts to reassign an existing label.
+command << "-v" + label
+//Args "-yv" 
+// Reassigns the version label specified by the -v option to the new revision, if the version
+// label exists.
 command << "-yv"
-//command << "-m TEST"
-//command << "-bp" + basePath
+
+//Args "-m"
+// Specifies the change description for the revision.
+// -mdescription specifies a description at the command-line.
+// To end the description, place a period (.) on a line by itself.
+changeDescription = "-m" + changeDescription
+command << "changeDescription"
+
+//Args "-bp"
+// Specifies the base project path to use in calculating workfile locations when -a or -o has
+// been specified. For multiple-file operations, path must be the entity path to a common
+// parent of each of the items being checked in.
+command << "-bp" + basePath
+
+//Args "-ym" 
+// Uses the change description specified by the -m option for all versioned items.
 command << "-ym"
+
+//Args "-nf" 
+// Aborts the Put operation if the workfile is unchanged or older.
 command << "-nf"
-command << "-z"
-// command << "lockPath"
+
 if (id != null) {
     command << "-id" + id
 }
+
 command << "-pr" + databasePath
 if (label != null && label.trim().length() > 0) {
     command << "-r" + label.trim()
@@ -99,9 +161,18 @@ else if (branch != null && branch.trim().length() > 0) {
 else if (promotionGroup != null && promotionGroup.trim().length() > 0) {
     command << "-g" + promotionGroup.trim() 
 }
+//Args "-a" 
+// Specifies an alternate location from which to check in workfiles, rather than the location to
+// which they were checked out. The check out location is used unless you specify either the
+// -a or -fw option
 command << "-a" + workDir.absolutePath
 command << projectPath
 
+
+//Args "-z" 
+// Includes revisions in subprojects.
+command << "-z" 
+command << unlockPath
 //------------------------------------------------------------------------------
 // EXECUTE
 //------------------------------------------------------------------------------
