b04d9a255af9b0b31cb9510fc7f8d96f40d7dc05
August-Icekimo <august.icekimo@gmail.com>
2020-02-13 10:56:17 +0800

    subfolder test

--- buildEmulator.ps1
+++ buildEmulator.ps1
@@ -1,9 +1,11 @@
+# 改變change.dat內容
 $out = new-object byte[] 4096; 
 (new-object Random).NextBytes($out); [IO.File]::WriteAllBytes('change.dat', $out)
+# 新增檔案
 $out = new-object byte[] 4096; 
-$names = -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
+$names = -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object {[char]$_})
 (new-object Random).NextBytes($out); [IO.File]::WriteAllBytes("Add-$names", $out)
 # 增加test子附錄下檔案
 $out = new-object byte[] 4096; 
-$names = -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
+$names = -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object {[char]$_})
 (new-object Random).NextBytes($out); [IO.File]::WriteAllBytes("test\Add-$names", $out)
